3f04c09267dadb1226c4c50bd63173cf4b7cee21
Rebecca Zee <ryz283@gmail.com>
2021-11-24 17:16:47 -0500

    T02.03-Exercise-DisplayUrl Gradle 7.0.3 compatible

--- Lesson02-GitHub-Repo-Search/T02.03-Exercise-DisplayUrl/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
+++ Lesson02-GitHub-Repo-Search/T02.03-Exercise-DisplayUrl/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
@@ -17,7 +17,7 @@ package com.example.android.datafrominternet;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
